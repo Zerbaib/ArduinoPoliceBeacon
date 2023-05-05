6779ea61c4eac230ce250f8de9be9f7664cdd1a7
Zerbaib <hugo.hennetin@proton.me>
2023-05-05 10:07:28 +0200

    set the var

---
+++ main/main.ino
@@ -0,0 +1,12 @@
+// Set var
+// Blue led
+const int b1 = 2;
+const int b2 = 3;
+const int b3 = 4;
+// White led
+const int w1 = 5;
+const int w2 = 6;
+// Red led
+const int r1 = 7;
+const int r2 = 8;
+const int r3 = 9;
